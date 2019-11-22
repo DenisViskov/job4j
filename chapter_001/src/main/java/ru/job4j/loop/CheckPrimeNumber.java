988a55e4a55a43beabb1a31b8b491843a74616d5
DenisViskov <denisviskov16@gmail.com>
2019-11-22 09:02:57 +0300

    Revert "5.5. Простое число.[#198205]".Исправлено 1.1

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,15 +3,10 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
 
     public boolean check(int finish) {
-        boolean prime = false;
-        for (int i = 1; i <= finish; i++) {
-            if (finish == 0) {
-                break;
-            } else if (finish % i == 0 && i != 1 && i != 5) {
+        boolean prime = true;
+        for (int i = 2; i < finish; i++) {
+            if (finish % i == 0) {
                 prime = false;
-                break;
-            } else {
-                prime = true;
             }
         }
         return prime;
