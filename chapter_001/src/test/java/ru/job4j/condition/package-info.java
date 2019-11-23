561f2d2883869209aeb7136ad91cc3300a4126de
DenisViskov <DenisViskov16@gmail.com>
2019-11-23 13:38:56 +0300

    Добавлены во все пакеты package-info.java

---
+++ chapter_001/src/test/java/ru/job4j/condition/package-info.java
@@ -0,0 +1,9 @@
+package ru.job4j.condition;
+
+/**
+ * Пакет содержащий тесты к домашним заданиям Condition
+ *
+ * @author Денис Висков (DenisViskov16@gmail.com)
+ * @version $1.0$
+ * @since 22.11.19
+ */
