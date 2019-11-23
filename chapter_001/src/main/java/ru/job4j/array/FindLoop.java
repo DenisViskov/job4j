ca53b629b455fb7110f73ff59cb6a0b33e2e5548
DenisViskov <DenisViskov16@gmail.com>
2019-11-23 18:09:07 +0300

    6.5. Поиск индекса в диапазоне.[#198220]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -4,7 +4,7 @@ package ru.job4j.array;
  * Класс осуществляющий поиск индекса в массиве с заданным элементом
  *
  * @author Денис Висков
- * @version 1.0
+ * @version 1.1
  * @since 23.11.19
  */
 public class FindLoop {
@@ -22,6 +22,26 @@ public class FindLoop {
             if (data[i] == el) {
                 rst = i;
                 break;
+            }
+        }
+        return rst;
+    }
+
+    /**
+     * Метод осуществляет поиск элемента в массиве с заданным диапазоном
+     *
+     * @param data   - Массив
+     * @param el     - элемент
+     * @param start  - индекс начала поиска
+     * @param finish - индекс завершения поиска
+     * @return - возвращает индекс найденного элемента, если элемента нет в заданном диапазоне
+     * вернёт -1
+     */
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int i = start; i <= finish; i++) {
+            if (data[i] == el) {
+                rst = i;
             }
         }
         return rst;
