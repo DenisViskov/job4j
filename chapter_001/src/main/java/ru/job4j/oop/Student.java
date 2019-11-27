c3303a81f17facd8a5c149c20a87fc48c2efeda0
DenisViskov <DenisViskov16@gmail.com>
2019-11-27 22:48:40 +0300

    1.2. Вызов метода объекта[#198339]

---
+++ chapter_001/src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,35 @@
+package ru.job4j.oop;
+
+/**
+ * Класс реализующий функционал исполнения музыки на баяне, а так же пения Студентом
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 27.11.2019
+ */
+public class Student {
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.music();
+        petya.music();
+        petya.music();
+        petya.song();
+        petya.song();
+        petya.song();
+    }
+
+    /**
+     * Метод реализует исполнение музыки на баяне
+     */
+    public void music() {
+        System.out.println("Tra tra tra");
+    }
+
+    /**
+     * Метод реализует исполнение песни
+     */
+    public void song() {
+        System.out.println("I believe I can fly");
+    }
+}
