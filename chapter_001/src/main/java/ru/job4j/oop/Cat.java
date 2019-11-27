c4d1f8ce37f092f64f2ff292bfb1735ef8a416c4
DenisViskov <DenisViskov16@gmail.com>
2019-11-27 22:42:14 +0300

    1.1. Создание объекта[#198337]

---
+++ chapter_001/src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,21 @@
+package ru.job4j.oop;
+
+/**
+ * Класс реализует создание объектов
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 27.11.2019
+ */
+public class Cat {
+
+    /**
+     * Метод в котором создаются два объекта
+     *
+     * @param args - Аргументы
+     */
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+/**
+ * Класс реализует создание объектов Dog
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 27.11.2019
+ */
+public class Dog {
+
+    /**
+     * Метод реализует создание трёх объектов типа Dog
+     *
+     * @param args - Аргументы
+     */
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
