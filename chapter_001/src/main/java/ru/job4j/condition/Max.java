53ad6233591d39b207e1c5f23bd7001466fb417d
DenisViskov <denisviskov16@gmail.com>
2019-11-28 17:00:58 +0300

    3. Перегрузить метод max для трех чисел[#198335]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -1,8 +1,53 @@
 package ru.job4j.condition;
 
+/**
+ * Класс реализует функционал сравнения нескольких чисел
+ *
+ * @author Денис Висков
+ * @version 1.1
+ * @since 28.11.2019
+ */
 public class Max {
-    public static int max(int left, int right) {
-        int result = left >= right ? left : right;
+
+    /**
+     * Метод реализует сравнение двух чисел
+     *
+     * @param first  - Первый аргумент
+     * @param second - Второй аргумент
+     * @return - максимум
+     */
+    public static int max(int first, int second) {
+        int result = first >= second ? first : second;
         return result;
+    }
+
+    /**
+     * Метод реализует сравнение трёх чисел
+     *
+     * @param first  - Первый аргумент
+     * @param second - Второй аргумент
+     * @param third  - Третий аргумент
+     * @return - максимум
+     */
+    public static int max(int first, int second, int third) {
+        int result = first >= second ? first : second;
+        int max = result >= third ? result : third;
+        return max;
+    }
+
+    /**
+     * Метод реализует сравнени четырёх чисел
+     *
+     * @param first  - Первый аргумент
+     * @param second - Второй аргумент
+     * @param third  - Третий аргумент
+     * @param four   - Четвёртый аргумент
+     * @return - максимум
+     */
+    public static int max(int first, int second, int third, int four) {
+        int one = first >= second ? first : second;
+        int two = third >= four ? third : four;
+        int max = one >= two ? one : two;
+        return max;
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -1,26 +1,27 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class MaxTest {
     @Test
-    public void whenMax1To2Then2() {
+    public void whenMaxIncludesTwoPosition() {
         int result = Max.max(1, 2);
         assertThat(result, is(2));
     }
 
     @Test
-    public void whenMax2To2Then1() {
-        int result = Max.max(3, 1);
-        assertThat(result, is(3));
+    public void whenMaxIncludesTreePosition() {
+        int result = Max.max(3, 1, 5);
+        assertThat(result, is(5));
     }
 
     @Test
-    public void whenMax1equallyMax2() {
-        int result = Max.max(3, 3);
-        assertThat(result, is(3));
+    public void whenMaxIncludesFourPosition() {
+        int result = Max.max(3, 3, 5, 7);
+        assertThat(result, is(7));
     }
 
 }
