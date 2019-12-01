a2e45ae4e5e4271b85cd12b316153e3131df4be8
DenisViskov <DenisViskov16@gmail.com>
2019-12-01 15:48:15 +0300

    2. Перегрузка конструктора.[#198350]

--- chapter_001/src/main/java/ru/job4j/oop/Animal.java
+++ chapter_001/src/main/java/ru/job4j/oop/Animal.java
@@ -4,12 +4,22 @@ package ru.job4j.oop;
  * Класс символизирующий абстракцию животного
  *
  * @author Денис Висков
- * @version 1.0
+ * @version 1.1
  * @since 01.12.2019
  */
 public class Animal {
-    public Animal() {
+    /**
+     * Имя животного
+     */
+    public String name;
+
+    public Animal(String name) {
         super();
+        this.name = name;
         System.out.println("Load Animal");
+    }
+
+    public Animal() {
+
     }
 }
