0b1a454c70d19050e52901e898fc2936dce49a4b
DenisViskov <denisviskov16@gmail.com>
2020-01-20 13:29:22 +0300

    update

---
+++ chapter_004/src/main/java/ru/job4j/streamapi/Matrix.java
@@ -0,0 +1,24 @@
+package ru.job4j.streamapi;
+
+import java.util.List;
+
+/**
+ * Class realizes function of changing integer matrix to List
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 20.01.2020
+ */
+public class Matrix {
+
+    /**
+     * Method realizes changing integer matrix to List
+     *
+     * @param matrix - matrix
+     * @return - List integer
+     */
+    public List matrixToList(Integer[][] matrix) {
+        //List<List<Integer>> result = Arrays.stream(matrix).flatMap(e -> Arrays.asList(e).stream()).collect(Collectors.toList());
+        return null;
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/streamapi/MatrixTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.streamapi;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class MatrixTest {
+    @Test
+    public void matrixTest() {
+        Integer[][] input = {{1, 2,}, {3, 4}};
+        List<Integer> expect = List.of(1, 2, 3, 4);
+        List<Integer> out = new Matrix().matrixToList(input);
+        Assert.assertEquals(expect, out);
+    }
+}
