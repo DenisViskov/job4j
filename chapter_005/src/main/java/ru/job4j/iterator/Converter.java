dd88accb36b1befea02d565e125a94a93b8b0bb3
DenisViskov <denisviskov16@gmail.com>
2020-01-31 23:06:46 +0300

    update

---
+++ chapter_005/src/main/java/ru/job4j/iterator/Converter.java
@@ -0,0 +1,57 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Class is converter of iterators to iterator
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 31.01.2020
+ */
+public class Converter {
+    /**
+     * Method has realize converter of iterators to iterator
+     *
+     * @param it - iterators
+     * @return - one iterator
+     */
+    public Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        return new Iterator<Integer>() {
+            Iterator<Integer> current = nextIncludedIterator();
+
+            @Override
+            public boolean hasNext() {
+                if (it.hasNext()) {
+                    return true;
+                } else {
+                    return current.hasNext();
+                }
+            }
+
+            @Override
+            public Integer next() {
+                if (current.hasNext()) {
+                    return current.next();
+                } else {
+                    current = nextIncludedIterator();
+                }
+                return current.next();
+            }
+
+            /**
+             * Method returns next included iterator
+             *
+             * @return - iterator
+             */
+            private Iterator<Integer> nextIncludedIterator() {
+                if (it.hasNext()) {
+                    return it.next();
+                } else {
+                    throw new NoSuchElementException();
+                }
+            }
+        };
+    }
+}
