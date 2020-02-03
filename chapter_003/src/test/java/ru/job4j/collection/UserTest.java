89de03252554fc0b557fd15d6e0eefd021043e54
DenisViskov <denisviskov16@gmail.com>
2020-02-03 12:13:22 +0300

    update

--- chapter_003/src/test/java/ru/job4j/collection/UserTest.java
+++ chapter_003/src/test/java/ru/job4j/collection/UserTest.java
@@ -2,22 +2,18 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
-import java.util.Iterator;
-import java.util.Set;
-
 import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class UserTest {
-    @Test
+    /*@Test
     public void whenAsc() {
         Set<User> users = Set.of(new User("Petr", 32),
                 new User("Ivan", 31));
         Iterator<User> it = users.iterator();
         assertThat(it.next(), is(new User("Ivan", 31)));
         assertThat(it.next(), is(new User("Petr", 32)));
-    }
+    }*/
 
     @Test
     public void whenComparePertVSIvan() {
