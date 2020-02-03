3aca2680b146ebb675028827cad42a56000786da
DenisViskov <denisviskov16@gmail.com>
2020-02-03 13:41:59 +0300

    update

---
+++ chapter_005/src/main/java/ru/job4j/generic/SimpleArray.java
@@ -0,0 +1,55 @@
+package ru.job4j.generic;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * Class has realize universal wrapper for array
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 03.02.2020
+ */
+public class SimpleArray<T> implements Iterable<T> {
+    /**
+     * Array
+     */
+    Object[] array;
+
+    /**
+     * Index
+     */
+    int index = 0;
+
+    public SimpleArray(int size) {
+        this.array = new Object[size];
+    }
+
+    /**
+     * Method has realize add element in array
+     *
+     * @param model - T element
+     */
+    public void add(T model) {
+        this.array[index++] = model;
+    }
+
+    /**
+     * Method has realize set model in array on the index
+     *
+     * @param index - index
+     * @param model - model
+     */
+    public void set(int index, T model) {
+        this.array[index] = model;
+    }
+
+    /**
+     * Method has remove element on index and shifts all remaining elements on the one position
+     *
+     * @param index - index
+     */
+    public void remove(int index) {
+
+    }
+}
