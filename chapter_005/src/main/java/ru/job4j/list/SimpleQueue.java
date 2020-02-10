dacb324e09a3baedc3218a754dfd2ba7b9796bc9
DenisViskov <denisviskov16@gmail.com>
2020-02-10 17:44:27 +0300

    update

---
+++ chapter_005/src/main/java/ru/job4j/list/SimpleQueue.java
@@ -0,0 +1,29 @@
+package ru.job4j.list;
+
+/**
+ * CLass has realize queue on base of stack
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 10.02.2020
+ */
+public class SimpleQueue<T> {
+
+    /**
+     * Method returns T element and remove his of queue
+     *
+     * @return - T
+     */
+    public T poll() {
+        return null;
+    }
+
+    /**
+     * Method has an add of gave element in queue
+     *
+     * @param value - T value
+     */
+    public void push(T value) {
+
+    }
+}
