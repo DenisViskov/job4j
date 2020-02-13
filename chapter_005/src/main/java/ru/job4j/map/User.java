507b927665cd823f4afeae3d4de676c8ab127498
DenisViskov <DenisViskov16@gmail.com>
2020-02-13 21:01:38 +0300

    1. Реализовать коллекцию Set на массиве[#229098]

---
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,33 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * Class has realize model of data user
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 13.02.2020
+ */
+public class User {
+    /**
+     * First name
+     */
+    private String name;
+
+    /**
+     * Children
+     */
+    private int children;
+
+    /**
+     * Birthday
+     */
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
