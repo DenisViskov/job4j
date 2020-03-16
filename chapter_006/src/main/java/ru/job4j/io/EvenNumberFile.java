885ed83068ec483a39f32fde65ab9b8555c666c0
DenisViskov <denisviskov16@gmail.com>
2020-03-16 10:34:18 +0300

    0. Видео[#229123]

---
+++ chapter_006/src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+/**
+ * Класс реализует ...
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 16.03.2020
+ */
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        StringBuilder text = new StringBuilder();
+        try (FileInputStream fileReader = new FileInputStream("even.txt")) {
+            int read;
+            while ((read = fileReader.read()) != -1) {
+                text.append((char) read);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String[] lines = text.toString().split(System.lineSeparator());
+        for (String line : lines) {
+            int value = Integer.valueOf(line);
+            boolean result = value % 2 == 0 ? true : false;
+            System.out.println(value + ": " + result);
+        }
+    }
+}
