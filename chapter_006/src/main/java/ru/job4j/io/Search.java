b22bf2007c99eddf632c92bd7672c7d253ba9f31
DenisViskov <denisviskov16@gmail.com>
2020-03-18 13:25:37 +0300

    update

---
+++ chapter_006/src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Класс реализует ...
+ *
+ * @author Денис Висков
+ * @version 1.0
+ * @since 18.03.2020
+ */
+public class Search {
+
+    public List<File> files(String parent, Predicate<File> condition) {
+        return null;
+    }
+}
---
+++ chapter_006/src/test/java/ru/job4j/io/SearchTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class SearchTest {
+
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void filesTest() {
+
+    }
+}
